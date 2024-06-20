fea5ae35781b32569f307b5db1ee450aa628bff9
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2024-06-19 19:25:57 -0500

    first commit prueba tecnica 55k Europa

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+const { parserOptions } = require("eslint-plugin-import/config/react");
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides:[],
+  parserOptions:{
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project:'./tsconfig.json'
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react-refresh',
+    'react'
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/explicit-function-return-type':'off',
+    'react/react-in-jsx-scope':'off'
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Prueba técnica 55k</h1>
+    </div>
+  )
+}
+
+export default App
