79db98f2945caedbf198f91dbf451f1f04b7f151
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2024-06-19 19:42:19 -0500

    hacer fetch con useEffect, useState, en App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [users, setUsers] = useState()
+
+  useEffect(()=>{
+    fetch('https://randomuser.me/api?results=100')
+    .then(res=> res.json())
+    .then(res=>{
+      setUsers(res.results)
+    })
+  })
   return (
     <div className="App">
       <h1>Prueba técnica 55k</h1>
