ceebae483451d840c8ee0f1ba59831d4c11fad01
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2024-07-14 12:07:27 -0500

    update app.tsx and index.css

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ Los pasos a seguir:
 [√] Provide the option to color rows as shown in the example. -  35.18
 [√] Allow the data to be sorted by country as demonstrated in the example.
 [√] Enable the ability to delete a row as shown in the example.
-[ ] Implement a feature that allows the user to restore the initial state, meaning that all deleted rows will be recovered.
-[ ] Handle any potential errors that may occur.
+[√]  Implement a feature that allows the user to restore the initial state, meaning that all deleted rows will be recovered.
+[√] Handle any potential errors that may occur.
 [ ] Implement a feature that allows the user to filter the data by country.
 [ ] Avoid sorting users again the data when the user is changing filter by 
 country.
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { UsersList } from './components/UsersList.tsx'
 import { type User } from './types/types.d'
@@ -8,6 +8,13 @@ function App() {
   const [users, setUsers] = useState<User[]>([])
   const [showColors, setShowColors] = useState(false)
   const [sortByCountry, setSortByCountry] = useState(false)
+  const [filterCountry, setFilterCountry] = useState<string | null>(null)
+
+  // useRef -> para guardar un valor que queremos que se comparta
+  // entre renderizados, pero que al cambiar, no vuelva a renderizar el componente 
+  // Implement a feature that allows the user to restore the initial state, 
+  // meaning that all deleted rows will be recovered.
+  const originalUsers = useRef<User[]>([])
 
   const toggleColors = () => {
     setShowColors(!showColors)
@@ -16,12 +23,15 @@ function App() {
     // esto se llama callback prevState=>!prevState
     setSortByCountry(prevState => !prevState)
   }
+  const handleReset = () => {
+    setUsers(originalUsers.current)
+  }
 
   const handleDelete = (email: string) => {
     // filteredUsers = users.filter((user, userIndex)
     // userIndex !== index
     const filteredUsers = users.filter((user) => user.email !== email)
-      setUsers(filteredUsers)
+    setUsers(filteredUsers)
   }
 
   useEffect(() => {
@@ -29,11 +39,21 @@ function App() {
       .then(res => res.json())
       .then(res => {
         setUsers(res.results)
+        originalUsers.current = res.results
       })
       .catch(err => {
         console.log(err)
       })
   }, [])
+
+  const filteredUsers = useMemo(() => {
+    console.log('calculate filteredUsers')
+    return filterCountry != null && filterCountry.length > 0
+      ? users.filter(user => {
+        return user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
+      })
+      : users
+  }, [users, filterCountry])
 
   // validacion ternaria
   const sortedUsers = sortByCountry
@@ -47,12 +67,19 @@ function App() {
     <div className="App">
       <h1>Prueba técnica 55k</h1>
       <header>
+
         <button onClick={toggleColors}>
           Color rows
         </button>
+
         <button onClick={toggleSortByCounrty}>
           {sortByCountry ? 'Contry-unSorted' : 'Coutry-Sorted'}
         </button>
+
+        <button onClick={handleReset}>
+          Reset state
+        </button>
+
       </header>
       <main>
         <UsersList
